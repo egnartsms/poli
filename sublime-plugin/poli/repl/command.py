1bb2e526db739548cec8a9320dff7f28286a5c8d
Serhii Mozghovyi <egnartsms@gmail.com>
2020-06-05 01:23:18 +0300

    Implement history navigation

--- sublime-plugin/poli/repl/command.py
+++ sublime-plugin/poli/repl/command.py
@@ -3,14 +3,16 @@ import sublime_plugin
 
 from poli.comm import comm
 from poli.exc import ReplEvalError
+from poli.repl.operation import History
 from poli.repl.operation import insert_prompt
 from poli.repl.operation import make_repl_view
 from poli.sublime import regedit
 from poli.sublime.misc import end_strip_region
 from poli.sublime.selection import set_selection
+from poli.sublime.view_assoc import make_view_assoc
 
 
-__all__ = ['PoliReplOpen', 'PoliReplSend', 'PoliReplClear']
+__all__ = ['PoliReplOpen', 'PoliReplSend', 'PoliReplClear', 'PoliReplPrev', 'PoliReplNext']
 
 
 class PoliReplOpen(sublime_plugin.WindowCommand):
@@ -53,9 +55,67 @@ class PoliReplSend(sublime_plugin.TextCommand):
 
         insert_prompt(self.view)
 
+        hns_for.pop(self.view)
+
 
 class PoliReplClear(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.set_read_only(False)
         self.view.erase(edit, sublime.Region(0, self.view.size()))
         insert_prompt(self.view)
+        hns_for.pop(self.view)
+
+
+hns_for = make_view_assoc()
+
+
+class HistoryNavigationState:
+    def __init__(self):
+        self.n_inputs_back = 0
+        self.pending_input = ''
+
+
+class PoliReplPrev(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if self.view in hns_for:
+            hns = hns_for[self.view]
+        else:
+            hns = hns_for[self.view] = HistoryNavigationState()
+
+        history = History(self.view)
+        if hns.n_inputs_back == history.number - 1:
+            sublime.status_message("Already at oldest input")
+            return
+
+        reg = regedit.editing_region(self.view)
+        if hns.n_inputs_back == 0:
+            hns.pending_input = self.view.substr(reg)
+        hns.n_inputs_back += 1
+        s = history.input(hns.n_inputs_back)
+        self.view.erase(edit, reg)
+        self.view.insert(edit, reg.begin(), s)
+        regedit.establish(self.view, sublime.Region(reg.begin(), reg.begin() + len(s)))
+
+
+class PoliReplNext(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if self.view in hns_for:
+            hns = hns_for[self.view]
+        else:
+            hns = hns_for[self.view] = HistoryNavigationState()
+
+        history = History(self.view)
+        if hns.n_inputs_back == 0:
+            sublime.status_message("Already at newest input")
+            return
+
+        reg = regedit.editing_region(self.view)
+        hns.n_inputs_back -= 1
+        if hns.n_inputs_back == 0:
+            s = hns.pending_input
+        else:
+            s = history.input(hns.n_inputs_back)
+
+        self.view.erase(edit, reg)
+        self.view.insert(edit, reg.begin(), s)
+        regedit.establish(self.view, sublime.Region(reg.begin(), reg.begin() + len(s)))
--- sublime-plugin/poli/repl/operation.py
+++ sublime-plugin/poli/repl/operation.py
@@ -20,3 +20,33 @@ def insert_prompt(view):
     set_selection(view, to=view.size())
     view.run_command('insert', {'characters': 'main> '})
     regedit.establish(view, sublime.Region(view.size()))
+
+
+class History:
+    """Helper class that knows where are prompt regions and result regions in a REPL"""
+
+    def __init__(self, view):
+        prompt_regs = view.find_by_selector('punctuation.separator.poli.repl.prompt')
+        result_regs = view.find_by_selector('punctuation.separator.poli.repl.result')
+        if len(prompt_regs) != len(result_regs) + 1:
+            sublime.error_message("REPL view is broken (number of prompts and results do "
+                                  "not match.")
+            raise RuntimeError
+
+        self.view = view
+        self.prompt_regs = prompt_regs
+        self.result_regs = result_regs
+
+    @property
+    def number(self):
+        return len(self.prompt_regs)
+
+    def input(self, n_inputs_back):
+        if n_inputs_back == 0:
+            reg = sublime.Region(self.prompt_regs[-1].end(), self.view.size())
+        else:
+            idx = len(self.prompt_regs) - 1 - n_inputs_back
+            reg = sublime.Region(self.prompt_regs[idx].end(),
+                                 self.result_regs[idx].begin() - 1)
+
+        return self.view.substr(reg)
