e5b49bdbf9cafadcdc3a30620117b54a967086b3
Serhii Mozghovyi <egnartsms@gmail.com>
2020-10-16 11:27:40 +0300

    bootstrap.js: cosmetic changes

--- backend/probe.lisp
+++ backend/probe.lisp
@@ -15,3 +15,24 @@
 
 (define process-furniture
   (* a b))
+
+(define delete-array-item 
+  (fn [ar i]
+    (assert (. obj2id (has ar)))
+
+    (. stmtDeleteProp (run #{[oid: (. obj2id (get ar))] [path: (jsonPath i)]}))
+    (. ar (splice i 1))))
+
+(define sample
+  (block
+    (let name "John")
+    (let age 25)
+    (let role "Sales manager")
+    #{[name: (+ name "!")] age role}
+    ))
+
+(define sample-2
+  (block
+    (let* [[fear "Hey"]
+           [gear "Fey"]]
+      (+ fear gear))))
