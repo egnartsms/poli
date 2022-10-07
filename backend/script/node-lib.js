f852cf434481b3b791212c78cc95dba0a2b82a87
Serhii Mozghovyi <egnartsms@gmail.com>
2022-10-07 20:40:28 +0300

    Simplify bootloader parsing code

--- backend/script/node-lib.js
+++ backend/script/node-lib.js
@@ -93,12 +93,15 @@ function parseModuleContents(str) {
 
 const reModuleName = /^[\w-]+$/;
 const reImportLine = /^(?<entry>\S+?)(?:\s+as:\s+(?<alias>\S+?))?$/;  // it's trimmed
+const reImportBlock = /^(?=\S)(?<donor>.+)\n(?<lines>(?:(?!\S).*\n?)*)/gm;
 
 
 function parseModuleImports(str) {
    let res = [];
 
-   for (let [[,donor], rawImports] of headerSplit(str, /^(?=\S)(.+?)\n/gm)) {
+   for (let match of str.matchAll(reImportBlock)) {
+      let {donor, lines: rawImports} = match.groups;
+
       if (!reModuleName.test(donor)) {
          throw new Error(`Bad module name to import: '${donor}'`);
       }
@@ -134,60 +137,26 @@ function parseModuleImports(str) {
 }
 
 
-const reDocstring = ` {3}:.*\\n(?:(?: *\\n)* {4,}\\S.*\\n)*`;
-const reDef = `(?:(?: *\\n)* .*\\n)*`;
+const reDocstring = `\x20{3}:.*\\n(?:(?:\x20*\\n)*\x20{4,}\\S.*\\n)*`;
+const reDef = `(?:(?:\x20*\\n)*\x20.*\\n)*`;
 const reBody = `(?<docstring>${reDocstring})?(?<def>${reDef})`;
 
 const reEntry = new RegExp(
-   `^(?<target>\\S.*?) +::=(?: *\\n(?<body>${reBody})| +(?<oneliner>.+)\\n)`,
+   `^(?<target>\\S.*?)\x20+:(?<kind>.*?):=(?:\x20*\\n(?<multiliner>${reBody})|\x20+(?<oneliner>.+)\\n)`,
    'gm'
 );
 
 
 function parseModuleBody(str) {
-   let entries = [];
-
-   for (let [mtch, interspace] of headerSplit(str, reEntry)) {
-      if (mtch === null) {
-         // Leading interspace is skipped
-         continue;
+   let entries = Array.from(str.matchAll(reEntry), match => {
+      return {
+         target: match.groups.target,
+         definition: match.groups.oneliner ?? match.groups.def,
+         kind: match.groups.kind || null
       }
-      
-      entries.push({
-         target: mtch.groups.target,
-         definition: mtch.groups.oneliner ?? mtch.groups.def
-      });
-   }
+   });
 
    return entries;
-}
-
-
-/**
- * Parse any kind of text separated with headers into header/body pairs:
-      ... HEADER ... HEADER ... HEADER ...
-
-   Everything following a header before the next header or the end of string is considered
-   a body that belongs to that header.
-
-   Yield pairs [header_match, body]. For the first header, if something precedes it, we
-   yield [null, body0].
-*/
-function* headerSplit(str, reHeader) {
-   let prev_i = 0, prev_mtch = null;
-
-   for (let mtch of str.matchAll(reHeader)) {
-      if (mtch.index > 0) {
-         yield [prev_mtch, str.slice(prev_i, mtch.index)];
-      }
-
-      prev_i = mtch.index + mtch[0].length;
-      prev_mtch = mtch;
-   }
-
-   if (prev_mtch !== null) {
-      yield [prev_mtch, str.slice(prev_i)];
-   }
 }
 
 class Queue {
